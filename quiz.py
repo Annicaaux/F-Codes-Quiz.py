5ee10881f050ef2aef656cd470b46e3e3595ba04
Annicaaux <klinke.annica@gmail.com>
2025-06-28 12:22:18 +0200

    Update quiz.py

--- quiz.py
+++ quiz.py
@@ -1,2 +1,14 @@
 import streamlit as st
+import datetime as datetime, time
+import random 
+
+
+# --Titel der Aplikation--
+st.set_page_config(
+    page_title="Ernie, Bernie, ich muss lernie sonst werd ich sterbie",
+    page_icon="🎓",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+# --Css-Stil 
 
